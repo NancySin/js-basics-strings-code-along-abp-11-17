0d00247c6e2b5a89365dc539dd481e618e718b6e
nancy sin <nsin@sbcglobal.net>
2017-11-14 04:20:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const welcomeMessage='Welcome to Flatbook, ' + currentUser+ '!';
 // const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage= welcomeMessage.toUpperCase();
 
-const shortGreeting=`Welcome, ${currentUser}`;
+const shortGreeting='Welcome currentUser';
+
